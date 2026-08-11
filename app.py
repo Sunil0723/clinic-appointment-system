9f9eb09821d1bc6d75ac47bea65d682e62c98a29
Sunil0723 <kawalkarsunil01@gmail.com>
2026-08-11 15:40:11 +0530

    Connect Flask to MySQL

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template
+import mysql.connector
 
 app = Flask(__name__)
+
+
+def get_db_connection():
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="YOUR_MYSQL_PASSWORD",
+        database="clinic_db"
+    )
+    return connection
 
 
 @app.route("/")
